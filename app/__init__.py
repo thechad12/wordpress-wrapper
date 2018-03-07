2a0114518a5b690c655c3978d47d45b67f4948d4
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-03-07 11:25:21 -0500

    minify html with flask package

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,14 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from flask_htmlmin import HTMLMIN
 
 app = Flask(__name__)
 app.config.from_object('config')
 login = LoginManager(app)
 login.login_view = 'login'
+app.config('MINIFY_PAGE') = True
+HTMLMIN(app)
 # Temporary database while app is in development/testing.
 # Switch to more secure database outside of app when in
 # production.
