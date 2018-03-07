c775d5491443a4ceb37ada511071e4f1a40cb4cf
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-03-07 13:54:03 -0500

    basic ajax request function for filtering

--- app/static/js/gulpfile.js
+++ app/static/js/gulpfile.js
@@ -1,7 +1,18 @@
+'use strict';
+
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify')
+	uglify = require('gulp-uglify'),
 	css = require('gulp-minify-css');
+
+/*************************************************
+* Gulp tasks
+* enter respective task in terminal to run
+* @function concat-scripts : Run gulp concat-scripts to concatenate
+* @function minify : Run gulp minify to minify javascripts
+*
+*
+***************************************************/
 
 gulp.task('concat-scripts', function() {
 	gulp.src('./scripts/*.js')
@@ -11,6 +22,8 @@ gulp.task('concat-scripts', function() {
 
 gulp.task('minify', function() {
 	gulp.src('./main/main.js')
-	.pipe(uglify())
+	.pipe(uglify().on('error', function(e) {
+		console.log(e);
+	}))
 	.pipe(gulp.dest('./main/main.min.js'));
 });
