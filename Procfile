554b92c3076c04b6c407314bfd77a56282158ea7
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-03-08 10:52:30 -0500

    edit procfile and gulp tasks to run on deploys

--- Procfile
+++ Procfile
@@ -1,2 +1,5 @@
 web: pipenv run python run.py
-init: python db_create.py
+init_db: python db_create.py
+init_modules: pipenv install
+init_js: npm install
+init_gulp: gulp heroku
--- app/static/js/gulpfile.js
+++ app/static/js/gulpfile.js
@@ -3,20 +3,25 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
-	css = require('gulp-minify-css');
+	css = require('gulp-minify-css'),
+	runSeq = require('run-sequence');
 
 /*************************************************
 * Gulp tasks
 * enter respective task in terminal to run
 * @function concat-scripts : Run gulp concat-scripts to concatenate
 * @function minify : Run gulp minify to minify javascripts
-*
+* @function concat-css : Concatenates CSS files
+* @function minify-css : Minifies CSS
+* @function heroku : Runs for production deploys
 *
 ***************************************************/
 
 gulp.task('concat-scripts', function() {
 	gulp.src('./scripts/*.js')
-	.pipe(concat('main.js'))
+	.pipe(concat('main.js').on('error', function(e) {
+		console.log(e);
+	}))
 	.pipe(gulp.dest('./main'));
 });
 
@@ -25,5 +30,28 @@ gulp.task('minify', function() {
 	.pipe(uglify().on('error', function(e) {
 		console.log(e);
 	}))
-	.pipe(gulp.dest('./main/main.min.js'));
+	.pipe(gulp.dest('./main'));
+});
+
+gulp.task('concat-css', function() {
+	gulp.scr('../css/*.css')
+	.pipe(concat('main.css').on('error', function(e) {
+		console.log(e);
+	}))
+	.pipe(gulp(dest('../css/main')));
+});
+
+gulp.task('minify-css', function() {
+	gulp.src('../css/main/main.css')
+	.pipe(uglify().on('error', function(e) {
+		console.log(e);
+	}))
+	.pipe(gulp.dest('../css/main'));
+});
+
+gulp.task('heroku', function() {
+	runSeq('concat-scripts', 'minify', 'concat-css', 'minify-css')
+	.on('error', function(e) {
+		console.log(e);
+	});
 });
