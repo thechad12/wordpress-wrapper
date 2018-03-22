ebd9e3792947a1e43a316819db336c1679b7b713
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-03-22 17:48:25 -0400

    create metadata for db table

--- db/db_create.py
+++ db/db_create.py
@@ -3,7 +3,7 @@ import os.path
 from config import SQLALCHEMY_DB_URI
 from app import db
 
-db.create_all()
+db.metadata.create_all()
 if not os.path.exists(SQLALCHEMY_DB_URI):
 	api.create(SQLALCHEMY_DB_URI, 'database repository')
 
