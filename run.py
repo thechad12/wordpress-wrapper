97b54de805ed6bb5b5324282c99c72933bfaa34a
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-03-22 16:41:42 -0400

    disable debug mode if deployed to heroku

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from app import app
 import os
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+if os.envrion.get('HEROKU') is None:
+	app.run(debug=True, host='0.0.0.0', port=port)
+else:
+	app.run(host='0.0.0.0', port=port)
