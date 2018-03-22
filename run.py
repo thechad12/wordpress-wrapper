5191f923da45cb117b4fced54261d66f9e409ea0
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-03-22 17:54:10 -0400

    fix syntax error

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
 import os
 port = int(os.environ.get('PORT', 5000))
-if os.envrion.get('HEROKU') is None:
+if os.environ.get('HEROKU') is None:
 	app.run(debug=True, host='0.0.0.0', port=port)
 else:
 	app.run(host='0.0.0.0', port=port)
