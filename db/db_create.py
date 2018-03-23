984193666a21987ffa2705e67b900bac9ccb0fda
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-03-23 17:32:07 -0400

    testing heroku build

--- db/db_create.py
+++ db/db_create.py
@@ -1,9 +1,9 @@
 from migrate.versioning import api
 import os.path
 from config import SQLALCHEMY_DB_URI
-from app import db
+from app import db, Base
 
-db.metadata.create_all()
+Base.metadata.create_all(db)
 if not os.path.exists(SQLALCHEMY_DB_URI):
 	api.create(SQLALCHEMY_DB_URI, 'database repository')
 
